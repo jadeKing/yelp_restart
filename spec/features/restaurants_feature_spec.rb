8333142d6a9db83f1591b172248f77c1b1af747c
Jade King <dev@jadeking.co.uk>
2015-05-07 15:11:17 +0100

    second feature tests migrations

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'no restaurants yet'
+      expect(page).to have_link 'add a restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'no restaurants yet'
+    end
+  end
+end
