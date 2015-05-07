08f7ed3b323f9a759ff4e5a0d9de81a39b06786d
Jade King <dev@jadeking.co.uk>
2015-05-07 23:47:47 +0100

    add delete restaurant feature

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -42,4 +42,28 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context '' do
+    before { Restaurant.create name: 'KFC'}
+
+    scenario 'user clicks delete and removes restaurant' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
