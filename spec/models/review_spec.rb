2f651a5c7a575f8bf3c8639e21f1e99ccf4539e6
Jade King <dev@jadeking.co.uk>
2015-05-08 20:09:50 +0100

    add feature rating can be no more than 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,6 +8,10 @@ describe Review, type: :model do
 
   it { should belong_to(:restaurant) }
 
+  it 'is invalid if rating is more than 5' do
+    review = described_class.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
   # it 'gets deleted if restaurant is deleted' do
   #   subject.should_belong_to(:restaurant)
   #   :kfc.destroy
